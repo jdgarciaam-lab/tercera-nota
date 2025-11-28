a862be335ca6a84bb6fb3a48be6ad4e761fce640
juangar <jdgarciaam@ufpso.edu.co>
2025-11-28 18:25:09 -0500

    nuevo

---
+++ src/UnidadUno/ValoresMagicos/ejemplo.java
@@ -0,0 +1,13 @@
+package UnidadUno.ValoresMagicos;
+
+public class ejemplo {
+   
+    public static void main(String[] args) {
+        double precioBase = 100;
+        double descuento = precioBase * 0.15; // Valor mágico
+        double iva = precioBase * 0.19; // Valor mágico
+        double precioFinal = precioBase - descuento + iva;
+        System.out.println("El precio final es: " + precioFinal);
+    }
+}
+
